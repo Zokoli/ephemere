1c92b7f4240727da7295e499de9401239080e7d1
Lapluiearrive <91179932+lapluiearrive@users.noreply.github.com>
2023-05-21 13:08:27 +0200

    Add files via upload

---
+++ votes.view.php
@@ -0,0 +1,40 @@
+<main id="regles">
+    <div class="page-content">
+        <h1 class="grostitre">À Postes Égaux, le jeu</h1>
+        <section class="perso-section perso-double row1">
+            <!-- premier paragraphe-->
+            <div class="perso-desc">
+                <p class="intro"><span class="tabulation">À Postes Égaux, du même nom que notre association, est un jeu de rôle de sensibilisation aux inégalités et discriminations entre les femmes et les hommes dans le monde professionnel. </span><br /><span class="tabulation">À travers ce jeu, nous souhaitons dénoncer toutes les discriminations qui passent souvent inaperçues, voire sont ignorées, mais nous espérons aussi participer à changer les mentalités et stéréotypes qui nuisent au quotidien à la vie professionnelle de nombreuses personnes.
+                    </span></p>
+            </div>
+            <div class="perso-card">
+                <img src="public/images/common/Hana.png" class="imgHanaJP" id="Hana" alt="image d'un personnage de notre jeu de rôle, Hana"><img src="public/images/common/JP.png" class="imgHanaJP" id="JP" alt="image d'un personnage de notre jeu de rôle, Jean-Pierre">
+            </div>
+        </section>
+
+        <h2 class="grostitre2">Comment jouer ?</h2>
+        <!--second paragraphe-->
+        <p class="Regles"><span class="tabulation">Tout comme dans n'importe quel jeu de rôle, vous incarnerez un personnage parmi la sélection proposée, chacun d'eux possède son propre nom, sa propre histoire. À travers ce jeu, vous vivrez différents scénarios dans le monde du travail, avec les points forts et contraintes de votre personnage. La conclusion en fin de partie : sommes nous tous égaux face au travail ?</span></p>
+
+        <h3 class="grostitre3">Quelques personnages</h3>
+        <section class="perso-section row1">
+            <!--troisième paragraphe-->
+            <div class="perso-card">
+                <img src="public/images/common/Hana.png" class="Hana" alt="image d'un personnage de notre jeu de rôle, Hana">
+            </div>
+            <div class="perso-desc">
+                <p>Hana, jeune mère de famille célibataire, est en recherche d'emploi depuis qu'elle s'est faite licenciée suite à une réorganisation de l'entreprise après son congé maternité.</p>
+                <p><br>Malheureusement, étant seule en charge de son bébé, aucune de ses demandes d'emploi n'aboutit. Personne ne se risque à l'engager puisque, après tout, elle doit bien s'occuper de son bébé, elle n'aura pas le temps de faire le travail demandé.</p>
+            </div>
+        </section>
+        <section class="perso-section row2">
+            <div class="perso-desc">
+                <p>Jean-Pierre, travaillant depuis une quarantaine d'années dans la même entreprise, ne croit pas qu'il existe une différence de traitement basée sur le genre. Pour lui, tout le monde est égal, et si les femmes sont en général moins bien payées que lui, c'est simplement parce qu'elles ne travaillent pas autant.</p>
+            </div>
+            <div class="perso-card">
+                <img src="public/images/common/JP.png" class="JP" alt="image d'un personnage de notre jeu de rôle, Jean-Pierre">
+            </div>
+        </section>
+        <div class="clear"></div>
+    </div>
+</main>
